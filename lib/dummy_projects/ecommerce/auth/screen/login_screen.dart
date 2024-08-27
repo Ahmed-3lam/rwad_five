87e5cba93c4f01b5462a8be55350b51900630546
Ahmed Allam <3lam.ahmedd@gmail.com>
2024-08-27 20:32:30 +0300

    Ecommerce Api (3)

--- lib/dummy_projects/ecommerce/auth/screen/login_screen.dart
+++ lib/dummy_projects/ecommerce/auth/screen/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:rwad/const.dart';
+import 'package:rwad/dummy_projects/ecommerce/auth/screen/register_screen.dart';
 
 import '../cubit/login_cubit.dart';
 import '../widgets/custom_textfield.dart';
@@ -43,86 +44,95 @@ class LoginScreen extends StatelessWidget {
           child: Center(
             child: Form(
               key: _formKey,
-              child: Column(
-                children: [
-                  Container(
-                    width: 300,
-                    child: Image.asset(
-                      imagePath + "logo2.png",
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 300,
+                      child: Image.asset(
+                        imagePath + "logo2.png",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: height * .1,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomTextfield(
-                        text: "Email",
-                        controller: _emailController,
-                        validator: (val) {
-                          final bool emailValid = RegExp(
-                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                              .hasMatch(val!);
-                          if (!emailValid) {
-                            return "This field should be a valid email";
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: height * .02,
-                      ),
-                      CustomTextfield(
-                        text: "Password",
-                        isPassword: true,
-                        controller: _passwordController,
-                        validator: (val) {
-                          if (val!.length < 6) {
-                            return "Password should be more than 6 letters";
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: height * .04,
-                      ),
-                      BlocBuilder<LoginCubit, LoginState>(
-                        builder: (context, state) {
-                          if (state is LoginLoadingState) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                          return InkWell(
-                            onTap: () {
-                              if (_formKey.currentState!.validate()) {
-                                context.read<LoginCubit>().login(
-                                      email: _emailController.text,
-                                      password: _passwordController.text,
-                                    );
-                              }
-                            },
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                color: Colors.green,
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
+                    SizedBox(
+                      height: height * .1,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomTextfield(
+                          text: "Email",
+                          controller: _emailController,
+                          validator: (val) {
+                            final bool emailValid = RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(val!);
+                            if (!emailValid) {
+                              return "This field should be a valid email";
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: height * .02,
+                        ),
+                        CustomTextfield(
+                          text: "Password",
+                          isPassword: true,
+                          controller: _passwordController,
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return "Password should be more than 6 letters";
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: height * .04,
+                        ),
+                        BlocBuilder<LoginCubit, LoginState>(
+                          builder: (context, state) {
+                            if (state is LoginLoadingState) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            return InkWell(
+                              onTap: () {
+                                if (_formKey.currentState!.validate()) {
+                                  context.read<LoginCubit>().login(
+                                        email: _emailController.text,
+                                        password: _passwordController.text,
+                                      );
+                                }
+                              },
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: Colors.green,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          );
-                        },
-                      )
-                    ],
-                  )
-                ],
+                            );
+                          },
+                        ),
+                        Center(
+                          child: TextButton(
+                              onPressed: () {
+                                Get.off(RegisterScreen());
+                              },
+                              child: Text("SignUp")),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
